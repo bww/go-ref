5790f8949b2969f466a07e1f32902f19b0efd164
Brian W. Wolter <brian@brianwolter.com>
2017-03-06 15:38:57 -0500

    Basic marshal roundtrip tests

--- test/data/basic/basic.go
+++ test/data/basic/basic.go
@@ -30,7 +30,7 @@ type W struct {
   B []*json.RawMessage  `json:"b" ref:"b_id,value"`
 }
 
-func TestMarshal(t *testing.T) {
+func TestMarshalRoundtrip(t *testing.T) {
   var s []byte
   var err error
   
@@ -42,11 +42,25 @@ func TestMarshal(t *testing.T) {
     assert.Equal(t, `{"a":123,"b":{"a":123}}`, string(s))
   }
   
+  var x1 X
+  err = json.Unmarshal(s, &x1)
+  if assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+    assert.Equal(t, x.A, x1.A)
+    assert.Equal(t, x.B, x1.B)
+  }
+  
   y := &Y{123, NewRawMessageRef(&m)}
   
   s, err = json.Marshal(y)
   if assert.Nil(t, err, fmt.Sprintf("%v", err)) {
     assert.Equal(t, `{"a":123}`, string(s))
+  }
+  
+  var y1 Y
+  err = json.Unmarshal(s, &y1)
+  if assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+    assert.Equal(t, y.A, y1.A)
+    assert.Equal(t, (*RawMessageRef)(nil), y1.B) // B is not marshaled
   }
   
   z := &Z{123, NewArrayOfRawMessageRef([]json.RawMessage{m, m})}
@@ -56,11 +70,25 @@ func TestMarshal(t *testing.T) {
     assert.Equal(t, `{"a":123,"b":[{"a":123},{"a":123}]}`, string(s))
   }
   
-  w := &Z{123, NewArrayOfRawMessageRef([]json.RawMessage{m, m})}
+  var z1 Z
+  err = json.Unmarshal(s, &z1)
+  if assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+    assert.Equal(t, z.A, z1.A)
+    assert.Equal(t, z.B, z1.B)
+  }
+  
+  w := &W{123, NewArrayOfPtrToRawMessageRef([]*json.RawMessage{&m, &m})}
   
   s, err = json.Marshal(w)
   if assert.Nil(t, err, fmt.Sprintf("%v", err)) {
     assert.Equal(t, `{"a":123,"b":[{"a":123},{"a":123}]}`, string(s))
+  }
+  
+  var w1 W
+  err = json.Unmarshal(s, &w1)
+  if assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+    assert.Equal(t, w.A, w1.A)
+    assert.Equal(t, w.B, w1.B)
   }
   
 }
